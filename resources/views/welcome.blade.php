07bafe9da35140c5f6c2bc40cd188623bd3313b3
David Antonio González <david.gonzalez5@tecnacional.edu.ni>
2023-10-20 16:21:37 -0600

    Primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="Male_Fashion Template">
+    <meta name="keywords" content="Male_Fashion, unica, creative, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Push Business</title>
+
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&display=swap"
+    rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="../assets/css/style.css" type="text/css">
+</head>
+
+<body>
+    <!-- Carga de antes -->
+    <div id="preloder">
+        <div class="loader"></div>
+    </div>
+
+    <!-- Menu del login responsive -->
+    <div class="offcanvas-menu-overlay"></div>
+    <div class="offcanvas-menu-wrapper">
+        <div id="mobile-menu-wrap"></div>
+        <div class="offcanvas__text">
+            <p>Estas a la mano de Push Business.</p>
+        </div>
+    </div>
+    <!-- Offcanvas Menu End -->
+
+    <!-- Header Section Begin -->
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__logo">
+                        <a href="#"><img src="../assets/img/logopush.png" alt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6">
+                    <nav class="header__menu mobile-menu">
+                        <ul>
+                            <li class="active"><a href="#">Inicio</a></li>
+                            <li><a href="#shop">Publicaciones</a></li>
+                            <li><a href="#socios">Socios</a></li>
+                            <li><a href="#planes">Planes</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="canvas__open"><i class="fa fa-bars"></i></div>
+        </div>
+    </header>
+    <!-- Header Section End -->
+
+    <!-- Hero Section Begin -->
+    <section class="hero">
+        <div class="hero__slider owl-carousel">
+            <div class="hero__items set-bg" data-setbg="../assets/img/hero/Home1.png">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-7 col-md-8">
+                            <div class="hero__text">
+                                <h6>BIENVENIDO A PUSH BUSINESS</h6>
+                                <h2>Potencia tu negocio</h2>
+                                <p>Descubre la plataforma que potencia tu negocio, donde proveedore, emprendedores
+                                    y clientes conectaran en un solo espacio.</p>
+                                <a href="login" class="primary-btn">Iniciar Sesión</a>
+                                <div class="hero__social">
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                    <a href="#"><i class="fa fa-pinterest"></i></a>
+                                    <a href="#"><i class="fa fa-instagram"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="hero__items set-bg" data-setbg="../assets/img/hero/Home3.png">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-5 col-lg-7 col-md-8">
+                            <div class="hero__text">
+                                <h6>Con PUSH BUSINESS</h6>
+                                <h2>Descubre el futuro del comercio</h2>
+                                <p>Realiza tus compras y ventas en un solo sitio. Administra y se parte de este
+                                    la revolución del comercio..</p>
+                                <a href="register" class="primary-btn">Registrate</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+
+    <br>
+
+    <!-- Product Section Begin -->
+    <section class="product spad" id="shop">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <ul class="filter__controls">
+                        <li class="active" data-filter="*">Realiza publicaciones</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row product__filter">
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c1.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Camiseta S,M,L</h6>
+                            <h5>C$350</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c2.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Sueter S,M</h6>
+                            <h5>C$450</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c3.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Sudadera (Negra), S,M</h6>
+                            <h5>C$320</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c4.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Bolsa G</h6>
+                            <h5>C$179</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c5.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Taza</h6>
+                            <h5>C$85</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item sale">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c6.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Termo metálico</h6>
+                            <h5>C$130</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix new-arrivals">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c7.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Cojin pequeño</h6>
+                            <h5>C$130</h5>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 col-sm-6 col-md-6 col-sm-6 mix hot-sales">
+                    <div class="product__item">
+                        <div class="product__item__pic set-bg" data-setbg="../assets/img/product/c8.jpg">
+                            <span class="label">Nuevo</span>
+                        </div>
+                        <div class="product__item__text">
+                            <h6>Gorra</h6>
+                            <h5>C$210</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Product Section End -->
+
+    <!-- Instagram Section Begin -->
+    <section class="instagram spad" id=socios>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="instagram__pic">
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/1.png"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/2.png"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/3.png"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/4.png"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/5.png"></div>
+                        <div class="instagram__pic__item set-bg" data-setbg="../assets/img/instagram/6.png"></div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="instagram__text">
+                        <h3>¡Sé parte de nuestros socios!</h3>
+                        <p>Disfruta de los privilegios de socios
+                            destacados, donde podras dar mayor
+                            visualización de tus productos y tener mayores posibilidades
+                            de ventas.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Instagram Section End -->
+
+    <!-- Latest Blog Section Begin -->
+    <section class="latest spad" id=planes>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-title">
+                        <h2>Planes</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="../assets/img/blog/PLANA.png"></div>
+                        <div class="blog__item__text">
+                            <h5>SIN ANUNCIOS</h5>
+                            <h5>Beneficios</h5>
+                            <p></p>
+                            <p>1.No veas los molestos anuncios.</p>
+                            <p>2.Ten una mejor experiencia navegando en nuestro sitio web</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic set-bg" data-setbg="../assets/img/blog/PLANP.png"></div>
+                        <div class="blog__item__text">
+                            <h5>PREMIUM</h5>
+                            <h5>Beneficios</h5>
+                            <p></p>
+                            <p>1.Acceso a inventarios (compra, venta y préstamos).</p>
+                            <p>2.Sé un cliente destacado, con mayor visibilidad.</p>
+                            <p>3.Verificación.</p>
+                            <p>4.Sin anuncios.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Latest Blog Section End -->
+
+    <!-- Footer Section Begin -->
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__logo">
+                            <a href="#"><img src="../assets/img/logospu.png" alt=""></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-2 offset-lg-1 col-md-3 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Redes sociales</h6>
+                        <ul>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Tiktok</a></li>
+                            <li><a href="#">Mail</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 offset-lg-1 col-md-6 col-sm-6">
+                    <div class="footer__widget">
+                        <h6>Contactanos</h6>
+                        <div class="footer__newslatter">
+                            <p>¿Tienes alguna consulta?</p>
+                            <form action="#">
+                                <input type="text" placeholder="Agrega tu correo">
+                                <button type="submit"><span class="icon_mail_alt"></span></button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <div class="footer__copyright__text">
+
+    <!-- Search Begin -->
+    <div class="search-model">
+        <div class="h-100 d-flex align-items-center justify-content-center">
+            <div class="search-close-switch">+</div>
+            <form class="search-model-form">
+                <input type="text" id="search-input" placeholder="Search here.....">
+            </form>
+        </div>
+    </div>
+    <!-- Search End -->
+
+    <!-- Js Plugins -->
+    <script src="../assets/js/jquery-3.3.1.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/jquery.nice-select.min.js"></script>
+    <script src="../assets/js/jquery.nicescroll.min.js"></script>
+    <script src="../assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="../assets/js/jquery.countdown.min.js"></script>
+    <script src="../assets/js/jquery.slicknav.js"></script>
+    <script src="../assets/js/mixitup.min.js"></script>
+    <script src="../assets/js/owl.carousel.min.js"></script>
+    <script src="../assets/js/main.js"></script>
+</body>
+
+</html>
